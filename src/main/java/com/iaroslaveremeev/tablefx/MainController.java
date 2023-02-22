ad1c92db25bd40f3a54971f8ae5fb5a94d045f10
Iaroslav <eremeev.pt@gmail.com>
2023-02-22 10:16:01 +0300

    Update MainController, add MailSender class

--- src/main/java/com/iaroslaveremeev/tablefx/MainController.java
+++ src/main/java/com/iaroslaveremeev/tablefx/MainController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
+import javafx.util.Callback;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,7 +16,7 @@ import java.util.Date;
 
 public class MainController {
 
-    public TableView<Object> table;
+    public TableView<User> table;
 
     @FXML
     public void openUsers(ActionEvent actionEvent) {
@@ -44,10 +45,38 @@ public class MainController {
                 mailCol.setCellValueFactory(new PropertyValueFactory<User, String>("mail"));
                 ageCol.setCellValueFactory(new PropertyValueFactory<User, Integer>("age"));
                 countryCol.setCellValueFactory(new PropertyValueFactory<User, String>("country"));
+                buttonCol.setCellValueFactory(new PropertyValueFactory<User, Button>("action"));
 
                 UserRepository userRepository = new UserRepository(file);
                 table.setItems(FXCollections.observableArrayList(userRepository.getUsers()));
-                table.getColumns().addAll(isSentCol, idCol, nameCol, regDateCol, mailCol, ageCol, countryCol, buttonCol);
+
+                Callback<TableColumn<User, Boolean>, TableCell<User, Boolean>> cellFactoryIsSent =
+                        new Callback<TableColumn<User, Boolean>, TableCell<User, Boolean>>() {
+                            @Override
+                            public TableCell call(final TableColumn<User, Boolean> param) {
+                                final TableCell<User, Boolean> cell = new TableCell<User, Boolean>() {
+                                    CheckBox btn = new CheckBox("Sent");
+                                    @Override
+                                    public void updateItem(Boolean item, boolean empty) {
+                                        super.updateItem(item, empty);
+                                        if (empty) {
+                                            setGraphic(null);
+                                            setText(null);
+                                        } else {
+                                            btn.setDisable(true);
+                                            btn.setStyle("-fx-opacity: 1;");
+                                            setGraphic(btn);
+                                            setText(null);
+                                        }
+                                    }
+                                };
+                                return cell;
+                            }
+                        };
+
+
+                //TODO 2 коллбека - за галочки и за отправку
+                table.getColumns().addAll(idCol, nameCol, regDateCol, mailCol, ageCol, countryCol);
             }
             else throw new FileNotFoundException();
         } catch (FileNotFoundException e) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.iaroslaveremeev.tablefx {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires javax.mail.api;
 
     opens com.iaroslaveremeev.tablefx to javafx.fxml;
     exports com.iaroslaveremeev.tablefx;
